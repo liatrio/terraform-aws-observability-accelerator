a64420fbff366c89960fdb5da4696339ce55fd8b
Paul Henson <paulh@liatrio.com>
2023-06-01 15:23:50 -0700

    Assigned output from grafana module to the locals for the accelerate module

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 data "aws_region" "current" {}
 
 data "aws_grafana_workspace" "this" {
-  workspace_id = var.managed_grafana_workspace_id
+  workspace_id = module.managed_grafana.workspace_id
 }
 
 locals {
