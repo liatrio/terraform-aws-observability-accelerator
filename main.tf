04f0081dece4bacf53bc00d3437344442a8b702e
Bota Tolepbergen <botatolepbergen@admins-MacBook-Pro.local>
2023-06-01 13:45:02 -0500

    added variables in variables.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 locals {
-  create_role   = var.create && var.create_iam_role
-  #iam_role_name = coalesce(var.iam_role_name, var.name)
+  name        = "aws-observability-accelerator"
+  description = "Amazon Managed Grafana workspace for ${local.name}"
 
-  #create_account_policy = local.create_role && var.account_access_type == "CURRENT_ACCOUNT"
-  #create_custom_policy  = length(setintersection(var.data_sources, ["CLOUDWATCH", "AMAZON_OPENSEARCH_SERVICE", "PROMETHEUS", "SNS"])) > 0
+  tags = {
+    GithubRepo = "terraform-aws-observability-accelerator"
+    GithubOrg  = "aws-observability"
+  }
 }
 
 
@@ -53,66 +55,36 @@ resource "grafana_folder" "this" {
   title = "Observability Accelerator Dashboards"
 }
 
-resource "aws_grafana_workspace" "this" {
-  count = var.create && var.create_workspace ? 1 : 0
+module "managed_grafana" {
+  source  = "terraform-aws-modules/managed-service-grafana/aws"
+  version = "1.8.0"
 
-  name        = var.name
-  description = var.description
+  name                      = local.name
+  associate_license         = false
+  description               = local.description
+  account_access_type       = "CURRENT_ACCOUNT"
+  authentication_providers  = ["AWS_SSO"]
+  permission_type           = "SERVICE_MANAGED"
+  data_sources              = ["CLOUDWATCH", "PROMETHEUS", "XRAY"]
+  notification_destinations = ["SNS"]
+  stack_set_name            = local.name
 
-  account_access_type      = var.account_access_type
-  authentication_providers = var.authentication_providers
-  permission_type          = var.permission_type
-
-  grafana_version           = var.grafana_version
-  configuration             = var.configuration
-  data_sources              = var.data_sources
-  notification_destinations = var.notification_destinations
-  organization_role_name    = var.organization_role_name
-  organizational_units      = var.organizational_units
-  role_arn                  = var.create_iam_role ? aws_iam_role.this[0].arn : var.iam_role_arn
-  stack_set_name            = coalesce(var.stack_set_name, var.name)
-
-  dynamic "vpc_configuration" {
-    for_each = length(var.vpc_configuration) > 0 ? [var.vpc_configuration] : []
-
-    content {
-      security_group_ids = var.create_security_group ? flatten(concat([aws_security_group.this[0].id], try(vpc_configuration.value.security_group_ids, []))) : vpc_configuration.value.security_group_ids
-      subnet_ids         = vpc_configuration.value.subnet_ids
+  configuration = jsonencode({
+    unifiedAlerting = {
+      enabled = true
     }
-  }
+  })
 
-  tags = var.tags
+
+  # Workspace IAM role
+  create_iam_role                = true
+  iam_role_name                  = local.name
+  use_iam_role_name_prefix       = true
+  iam_role_description           = local.description
+  iam_role_path                  = "/grafana/"
+  iam_role_force_detach_policies = true
+  iam_role_max_session_duration  = 7200
+  iam_role_tags                  = local.tags
+
+  tags = local.tags
 }
-
-resource "aws_iam_role" "this" {
-  count = local.create_role ? 1 : 0
-
-  name        = var.use_iam_role_name_prefix ? null : local.iam_role_name
-  name_prefix = var.use_iam_role_name_prefix ? "${local.iam_role_name}-" : null
-  description = var.iam_role_description
-  path        = var.iam_role_path
-
-  assume_role_policy    = data.aws_iam_policy_document.assume[0].json
-  force_detach_policies = var.iam_role_force_detach_policies
-  max_session_duration  = var.iam_role_max_session_duration
-  permissions_boundary  = var.iam_role_permissions_boundary
-
-  tags = merge(var.tags, var.iam_role_tags)
-}
-
-data "aws_iam_policy_document" "assume" {
-  count = local.create_role ? 1 : 0
-
-  statement {
-    sid     = "GrafanaAssume"
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["grafana.${data.aws_partition.current.dns_suffix}"]
-    }
-  }
-}
-
-data "aws_partition" "current" {}
